8307a19867da8e0d62780b235e1a3b8846ccafb7
Ayush <giriayush624@gmail.com>
2026-06-08 10:14:29 +0530

    fix port binding

--- server.js
+++ server.js
@@ -12,8 +12,20 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 10000,
-  family: 4
-})
-  .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("CampusKart API is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err.message);
+    process.exit(1);
+  });
